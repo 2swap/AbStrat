b70890a7db08e6bf1bb4582317692212f8629b25
2swap <mchontz10@gmail.com>
2020-04-26 14:21:32 -0400

    Worked on kalah, it still doesn't compile yet, getting there.

--- kalah.cpp
+++ kalah.cpp
@@ -5,47 +5,103 @@
 #include <unistd.h>
 #include <string>
 #include <map>
-using namespace std;
-int* realBoard;
-char alph [4] = {' ','|','-','+'};
-string players;
-int m=6, n=1, h=m*2+2;
 
+using namespace std;
+
+int* realBoard;
+
+const char alph [4] = {' ','|','-','+'};
+string players;
+
+const int m=6;						// holes per side
+const int n=1;						// counters per hole
+const int h=m*2+2;					// total number of pits in board
+const int northsKalahah = m*2+1;	// Array index of North's large pit
+const int southsKalahah = m;		// Array index of South's large pit
+const int totalStones = n*m*2;		// Amount of stones on board in total
+
+
+
+
+//MATH
+//MATH
+//MATH
+//MATH
+//MATH
+
+// Returns whether index x is a kalahah or not
+bool isKalahah(int x){
+	return x == northsKalahah || x == southsKalahah;
+}
+
+// Returns whether index x is a small south pit or kalahah
+bool isSouthSide(int x){
+	return x < southsKalahah;
+}
+
+// Returns whether index x is a small south pit or kalahah
+bool isNorthSide(int x){
+	return x > southsKalahah && x < northsKalahah;
+}
+
+
+
+
+
+
+
+//BOARDS
+//BOARDS
+//BOARDS
+//BOARDS
+//BOARDS
+
+// Makes an empty board
 int* createBoard(){
-	int* board = new int*[m*2+2];
-	for(int i = 0; i < h; ++i) if(i != m && i != m*2+1) board[y] = n;
+	int* board = new int[h];
+	for(int i = 0; i < h; ++i) if(!isKalahah(i)) board[i] = n;
 	return board;
 }
 
+// Safely deletes a board
+void deleteBoard(int* board){
+	delete [] board;
+}
+
+// Returns a deep copy of the input board
 int* copyBoard(int* copy){
-	int* board = new int*[h];
+	int* board = new int[h];
 	for (int i = 0; i < h; ++i) board[i] = copy[i];
 	return board;
 }
 
+// Is the game over?
 bool isComplete(int* board){
-	return board[m]+board[2*m+1]==n*m*2;
+	return board[southsKalahah] + board[northsKalahah] == totalStones;
 }
 
+// Heuristic in which positive favors South
 int heuristic(int* board){
-	return board[m]-board[2*m+1];
+	return board[southsKalahah] - board[northsKalahah];
 }
 
+// Says the winner
 char whoWon(int* board){
 	if (!isComplete(board)) return ' '; // Game isn't over!
-	if (board[m]<board[m*2+1]) return 'N'; // North (Second Player) won!
-	if (board[m]>board[m*2+1]) return 'S'; // South (First Player) won!
+	if (board[southsKalahah]>board[northsKalahah]) return 'S'; // South ( First Player) won!
+	if (board[southsKalahah]<board[northsKalahah]) return 'N'; // North (Second Player) won!
 	return 'T'; // It's a complete tie game!
 }
 
+// Prints a bar without newlines. ct=2 --> +---+---+
 void printVBar(int ct){
-	for(int i = 0; i < ct; i++) cout << "+---";
+	for(int i = 0; i < ct; ++i) cout << "+---";
 	cout << '+';
 }
 
+// Prints a nice looking ascii board
 void printBoard(int* board){
 	cout << endl;
-
 
 	printVBar(m+2);
 
@@ -53,9 +109,9 @@ void printBoard(int* board){
 	for(int x = 2*m; x > m; --x) cout << ' ' << board[x] << " +";
 	cout << endl;
 
-	cout << "+ " << board[2m+1] << " +";
+	cout << "+ " << board[northsKalahah] << " +";
 	printVBar(m);
-	cout << "+ " << board[m] << " +" << endl;
+	cout << "+ " << board[southsKalahah] << " +" << endl;
 
 	cout << "+   + ";
 	for(int x = 0; x < m; ++x) cout << ' ' << board[x] << " +";
@@ -63,31 +119,42 @@ void printBoard(int* board){
 
 	printVBar(m+2);
 
-
 	cout << endl << ' ';
-	for(int x = 0; x < w; x++) cout << ' ' << (x+1) << "  ";
+	for(int x = 0; x < m; x++) cout << ' ' << (x+1) << "  ";
 	cout << endl << endl; 
 }
 
-void deleteBoard(int* board){
-	delete [] board;
-}
+// Picks up the stones at location x and plays them.
+void play(int* board, int x, bool turn){
 
-void play(int* board, int x){
-	assert(x != m && x != 2*m+1);
+	// You can't play from the kalahahs
+	assert(x != northsKalahah && x != southsKalahah);
+	
+	// You must play from your own side
+	if( turn) assert(isSouthSide(x));
+	if(!turn) assert(isNorthSide(x));
+
 	int ct = board[x];
 	int hand = x;
 	while(ct > 0){
-		hand++;
-		board[hand%(2*m+2)]++;
+		hand=(hand+1)%h;
+		if(hand == (turn?northsKalahah:southsKalahah)) hand=(hand+1)%h; // Don't place in opponent's kalahah
+		board[hand]++;
 		ct--;
+	}
+
+	// Capture
+	if(!isKalahah(hand) && board[hand]==1 && board[(hand+h/2)%h]!=0){
+		board[hand] = 0;
+		board[(turn?southsKalahah:northsKalahah)] += 1+board[(hand+h/2)%h];
+		board[(hand+h/2)%h] = 0;
 	}
 }
 
-//1 if the player who just went is winning.
+// 1 if the player who just went is winning.
 int alphabeta(int* board, int depth, double alpha, double beta, bool turn){
 	if(depth == 0 || isComplete(board)) return heuristic(board);
-	int value;
+	double value;
 	if(turn){
 		value = -10000;
 		for(int i = 0; i < m; i++){
@@ -109,15 +176,14 @@ int alphabeta(int* board, int depth, double alpha, double beta, bool turn){
 }//call: alphabeta(origin, depth, -inf, +inf, ?true?)
 
 int robotMove(int* board, char turn, int depth){
-	//sleep(1);
-	//cache.clear();
-	int max = -1000000;
+	sleep(1);
+	int max = -100000;
 	int spot = -1;
-	for(int x = 0; x < w; x++) {
-		if(board[h-1][x] != ' ') continue;
+	for(int x = 0; x < m; x++) {
+		if (board[x] != ' ') continue;
 		int* playHere = copyBoard(board);
 		play(playHere,x,turn);
-		int eval = evaluate(playHere, turn, depth);
+		int eval = alphabeta(playHere, depth, -100000000, 100000000, turn);
 		deleteBoard(playHere);
 		cout << " " << eval << " ";
 		if(eval>max){
@@ -136,32 +202,28 @@ int main(){
 	cout << "Player Types?" << endl;
 	cin >> players;
 
+	//true  == first to move  == south
+	//false == second to move == north
 	bool turn = true;
 	
 	while(true){
 		int x = -1;
-		printBoard();
+		printBoard(realBoard);
 		cout << turn << ": ";
-		if(players[turnNo%2] == 'h') {cin >> x; x--;}
-		if(players[turnNo%2] == 'r' || x>100) {x = robotMove(realBoard, turn, 0);}
-		if(x < 0 || x >= w || realBoard[h-1][x] != ' ') {
+		if(players[turn] == 'h') {cin >> x; x--;}
+		if(players[turn] == 'r' || x>100) {x = robotMove(realBoard, turn, 0);}
+		if(x < 0 || x >= h || realBoard[x] != ' ') {
 			cout << "invalid."  << endl;
 			break;
 		}
 		play(realBoard,x,turn);
 		char win = whoWon(realBoard);
 		if(win != ' '){
-			printBoard();
-			cout << "Game Over! " << turn << " Wins!" << endl;
+			printBoard(realBoard);
+			cout << "Game Over! " << (players[turn]==h?"Human":"Robot") << " Wins!" << endl;
 			break;
 		}
-		turn = otherTurn(turn);
-		turnNo++;
-		if(turnNo == w*h){
-			printBoard();
-			cout << "Game Over! It's a tie!" << endl;
-			break;
-		}
+		turn = !turn;
 	}
 
 	deleteBoard(realBoard);
