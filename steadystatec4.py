ff53ff11fe619e1a3550268e7ceb13554ea72df2
Tu Nombre <you@example.com>
2023-04-02 08:53:26 -0400

    more steadystates

--- steadystatec4.py
+++ steadystatec4.py
@@ -4,40 +4,24 @@ import time, random
 boardheight = 6
 boardwidth = 7
 priority_list = ["+", " ", "-"]
-miai = ['@','#']
+miai = ['@','#', '$']
 steadystates = [[
-        list("       "),
-        list(" 21    "),
-        list(" 12    "),
-        list(" 21    "),
-        list(" 122   "),
-        list(" 121   ")
+        list("+-+@-#-"),
+        list("-+-2+1+"),
+        list("+-21-2-"),
+        list("-+12+1+"),
+        list("$-21-2-"),
+        list("2@12$1#")
 ]]*1000
 ssindex = 0
 known_steadystates = [
         [
-        list("*  2   "),
-        list("-**1 1*"),
-        list("*--2 2-"),
-        list("-**1*1*"),
-        list("*- 2-2-"),
-        list("- 12 21")
-        ],
-        [
-        list("+++ +++"), # The original!
+        list("+++c+++"), # The original!
         list("---2---"),
         list("+++1+++"),
         list("---2---"),
-        list("++11++ "),
-        list("--21-2+")
-        ],
-        [
-        list("+++ +++"),
-        list("---+---"),
-        list("+++-+++"),
-        list("---2---"),
-        list("++11++ "),
-        list("--21-2+")
+        list("++11+ad"),
+        list("--21-2b")
         ],
         [
         list("*******"), # woah...
@@ -46,14 +30,6 @@ known_steadystates = [
         list("-12--++"),
         list("#21**  "),
         list("212--@@")
-        ],
-        [
-        list("       "),
-        list(" 21    "),
-        list(" 12    "),
-        list(" 21    "),
-        list(" 122   "),
-        list(" 121   ")
         ],
         [
         list("++111++"), # First found computationally!
@@ -72,19 +48,21 @@ known_steadystates = [
         list("--21--2")
         ],
         [
-        list("*      "),
-        list("-**   *"),
-        list("*--   -"),
-        list("-** 1 *"),
-        list("*-2 21-"),
-        list("- 12122")
+        list("+ 1+- *"),
+        list("--2 +* "),
+        list("+11 *+*"),
+        list("*222*+*"),
+        list("-211-* "),
+        list("1122 -+")
         ],
-        [['+', ' ', '1', '+', '-', ' ', '*'],
-         ['-', '-', '2', ' ', '+', '*', ' '],
-         ['+', '1', '1', ' ', '*', '+', '*'],
-         ['*', '2', '2', '2', '*', '+', '*'],
-         ['-', '2', '1', '1', '-', '*', ' '],
-         ['1', '1', '2', '2', ' ', '-', '+']]
+        [
+        list("   2++ "), # good but not perfect
+        list("  -2*1 "),
+        list("+ 21-2 "),
+        list("-*12+1*"),
+        list("+-21-2-"),
+        list("2112+1+")
+        ]
 ]
 
 def generate_board():
@@ -118,13 +96,17 @@ def mark_board(steadystate, board, y, x, player):
 
 def play(steadystate, board):
     #column = int(input("Pick a column (1-7): ")) - 1
-    x = random.randint(0,6)
-    while board[0][x] != ".":
-        x = random.randint(0,6)
+    x = -1
+    indices = [i for i, x in enumerate(board[0]) if x == '.'] # get indices of all legal moves
+    if indices: # if there is a legal move
+        x = random.choice(indices) # choose a random index from the list of indices
+    else:
+        return False # There are no legal moves.
     for y in range(boardheight-1,-1,-1):
         if board[y][x] == ".":
             mark_board(steadystate, board, y, x, 1)
-            return
+            return True
+    return False
 
 def steadystateresponse(steadystate):
     alph = {}
@@ -134,12 +116,17 @@ def steadystateresponse(steadystate):
             if letter in miai:
                 alph[letter] = alph.get(letter, 0) + 1
     for key in alph:
+        if alph[key] > 2:
+            return False
         if alph[key] == 1:
             for x in range(boardwidth):
                 for y in range(boardheight):
                     if steadystate[y][x] == key:
+                        if y!=boardheight-1 and board[y+1][x]=='.':
+                            return False
                         mark_board(steadystate, board, y, x, 2)
-                        return
+                        return True
+            return False
 
     priorities = ["x"]*boardwidth
     for x in range(boardwidth):
@@ -154,16 +141,13 @@ def steadystateresponse(steadystate):
             x = priorities.index(i)
             break
 
+    y = -1
     for i in range(boardheight):
         if board[i][x] == ".":
             y = i
+    if y == -1 or x == -1:
+        return False
     mark_board(steadystate, board, y, x, 2)
-
-def tie(board):
-    for y in range(boardheight):
-        for x in range(boardwidth):
-            if board[y][x] not in ['1','2']:
-                return False
     return True
 
 def check_winner(board, player):
@@ -194,19 +178,23 @@ def check_winner(board, player):
     return False
 
 def onwin(ssindex):
-    if(random.random() < 0.94):
-        return
     whichoverwrite = random.randint(0,len(steadystates)-1)
-    while(whichoverwrite == ssindex):
+    while whichoverwrite == ssindex:
         whichoverwrite = random.randint(0,len(steadystates)-1)
-    steadystates[whichoverwrite] = mutate(steadystates[ssindex])
+    r = random.random()
+    if r < 0.2:
+        y = random.randint(0, boardheight-1)
+        x = random.randint(0, boardwidth-1)
+        steadystates[whichoverwrite][y][x] = steadystates[ssindex][y][x]
+    elif r<0.25:
+        steadystates[whichoverwrite] = mutate(steadystates[ssindex])
 
 def mutate(ss):
     (ret,_) = generate_board()
     for y in range(boardheight):
         for x in range(boardwidth):
             if ret[y][x] in priority_list and random.random()<0.05:
-                ret[y][x] = random.choice(priority_list)
+                ret[y][x] = random.choice(priority_list + miai)
     return ret
 
 while True:
@@ -216,10 +204,12 @@ while True:
         (steadystate, board) = generate_board()
         won = False
         while True:
-            play(steadystate, board)
-            if check_winner(board, "1") or tie(board):
+            if not play(steadystate, board):
                 break
-            steadystateresponse(steadystate)
+            if check_winner(board, "1"):
+                break
+            if not steadystateresponse(steadystate):
+                break
             if check_winner(board, "2"):
                 wins+=1
                 won = True
@@ -227,12 +217,9 @@ while True:
                     print(f"SteadyState wins! {wins}")
                 if wins % 10000 == 0:
                     pprint(steadystates[ssindex])
-                    pprint(steadystate)
                 if wins == 100000:
                     exit()
                 onwin(ssindex)
-                break
-            if tie(board):
                 break
         if not won:
             print(f"wins: {wins}")
